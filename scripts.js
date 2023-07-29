d95ec2743586ca94016b0cb87a6bead634d9ec06
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-07-29 16:02:09 +0100

    add swiping transition for cards

--- scripts.js
+++ scripts.js
@@ -180,3 +180,43 @@ function validateAndClearForm() {
         form.reset();
     }
 };
+
+/*
+Regarding card swiping animations 
+*/
+
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card");
+
+const handleSwipeRight = () => {
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+  
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+        
+  currentGroup.dataset.status = "after";
+  
+  nextGroup.dataset.status = "becoming-active-from-before";
+  
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const handleSwipeLeft = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+  
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentGroup.dataset.status = "before";
+  
+  nextGroup.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
