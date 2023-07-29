13b8b578906fbe8734c7d1099cd164075cd76e26
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-07-29 13:27:34 +0100

    split function adding to array/add spines on page
    Separated the function to add some maths so that the max amount of spines can be appended to the appropriate shelves

--- scripts.js
+++ scripts.js
@@ -6,10 +6,8 @@
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-      return `${title} by ${author} has ${pages} pages, ${read}.`
-    };
   };
+
 
   function addBookToLibrary() {
 
@@ -23,33 +21,62 @@
     } else {
         read = "you have read this book";
     }
-    
-    console.log(title);
-    console.log(author);
-    console.log(pages);
-    console.log(read);
 
-    const commitBook = new book(title, author, pages, read)
-    myLibrary.push(commitBook)
-    console.log(myLibrary)
+    const commitBook = new book(title, author, pages, read);
+    myLibrary.push(commitBook);
+    console.log(myLibrary);
+  };
 
+// Below is code to calculate how many spines can fit onto each shelf and handles the logic for selecting shelves that have space to accept more spines
+ 
+const spineWidth = 55 + 3.2;  // this is the border-box size + the 'gap' value on the shelf
+
+function getMaxSpinesForShelf(shelf) {
+    return (Math.floor(shelf.offsetWidth / spineWidth));
+}
+
+function getAvailableShelf() {
+    const shelves = document.querySelectorAll('.shelf');
+    for (let shelf of shelves) {
+        const currentSpines = shelf.querySelectorAll('.spine').length;
+        const maxSpines = getMaxSpinesForShelf(shelf);
+
+        console.log(`Shelf: ${shelf}, Current spines: ${currentSpines}, Max spines: ${maxSpines}`); 
+
+        if (currentSpines < maxSpines) {
+            return shelf;
+        }
+    }
+    alert("There no more space on the shelves! Your book has been added to storage.")
+}
+
+function addSpineToShelf() {
+    let title = document.getElementById('book-title').value;
+    let read;
+    const readValidation = document.getElementById('read-validation');
+    if (readValidation.checked == false) {
+        read = "you have not read this book";
+    } else {
+        read = "you have read this book";
+    }
     let spine = document.createElement('div');
     spine.className = 'spine';
-    
+
     let spineText = document.createElement('p');
+    if (read === "you have not read this book") {
+        spineText.style.color = "var(--clr-white)";
+    } else {
+        spineText.style.color = "var(--clr-black)";
+    }
     spineText.textContent = `${title}`;
     spine.appendChild(spineText);
-    
+
     if (read === "you have not read this book") {
         spine.style.backgroundColor = "var(--clr-not-read)";
     } else {
         spine.style.backgroundColor = "var(--clr-read)";
     }
-    
-    const shelf = document.querySelector('.shelf');
-    shelf.appendChild(spine);
-  };
 
-
-
-
+    const targetShelf = getAvailableShelf();
+    targetShelf.appendChild(spine);
+};
